cccc23a272405971ed3ac0199c68f95238d63010
AdrianSzromba <adrian.szromba@gmail.com>
2017-12-01 18:20:23 +0100

    export methods from Group to GroupDAO

--- src/pl/coderslab/warsztat3/model/Group.java
+++ src/pl/coderslab/warsztat3/model/Group.java
@@ -1,10 +1,5 @@
 package pl.coderslab.warsztat3.model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Group {
 	
 	private int id;
@@ -34,40 +29,7 @@ public class Group {
 		return id;
 	}
 
-	public void saveToDB(Connection conn) throws SQLException {
-		if(this.id == 0) {
-			String sql = "INSERT INTO user_group(name) VALUES(?);";
-			String[] generatedColumns ={"ID"};
-			PreparedStatement ps = conn.prepareStatement(sql, generatedColumns);
-			ps.setString(1, this.name);
-			ps.executeUpdate();
-			ResultSet resultSet = ps.getGeneratedKeys();
-			if (resultSet.next()) {
-				this.id = resultSet.getInt(1);
-			}
-			resultSet.close();
-			ps.close();
-		} else {
-			String sql = "UPDATE user_group SET name=? WHERE id=?;";
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, this.name);
-			ps.setLong(2, this.id);
-			ps.executeUpdate();
-			ps.close();
-		}
-	}
-	
-	static public Group loadGroupById(Connection conn, int id) throws SQLException {
-		String sql = "SELECT * FROM user_group where id=?;";
-		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setInt(1, id);
-		ResultSet resultSet = ps.executeQuery();
-		if (resultSet.next()) {
-			Group loadedGroup = new Group();
-			loadedGroup.id = resultSet.getInt("id");
-			loadedGroup.name = resultSet.getString("name");
-			return loadedGroup;
-		}
-		return null;
+	public void setId(int id) {
+		this.id = id;
 	}
 }
