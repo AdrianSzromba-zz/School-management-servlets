c1c9ba9ea1f9d3d8b2a3a8fe88eebb883af0c86b
AdrianSzromba <adrian.szromba@gmail.com>
2017-12-01 11:46:08 +0100

    add rest of databases to system

--- sql/db.sql
+++ sql/db.sql
@@ -1,16 +1,34 @@
-CREATE TABLE user (
+CREATE TABLE user_group (
 	id INT AUTO_INCREMENT,
 	name VARCHAR(255),
-	email VARCHAR(255),
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
 
+CREATE TABLE users (
+	id BIGINT(20) AUTO_INCREMENT,
+	username VARCHAR(255),
+	email VARCHAR(255) UNIQUE,
+	password VARCHAR(245),
+	user_group_id INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(user_group_id) REFERENCES user_group(id)
+);
+
+CREATE TABLE excercise (
+	id INT AUTO_INCREMENT,
+	title VARCHAR(255),
+	description TEXT,
+	PRIMARY KEY(id)
+);
 
 CREATE TABLE solution (
 	id INT AUTO_INCREMENT,
-	title VARCHAR(255),
-	submition_date DATETIME,
-	user_id INT,
+	created DATETIME,
+	updated DATETIME,
+	description TEXT,
+	excercise_id INT,
+	users_id BIGINT,
 	PRIMARY KEY(id),
-	FOREIGN KEY(user_id) REFERENCES user(id)
+	FOREIGN KEY(users_id) REFERENCES users(id),
+	FOREIGN KEY(excercise_id) REFERENCES excercise(id)
 );
